e8232934c1a70fb0f97dc7d0ecad200e1ec9c3e4
kotmarvell <marvelkot111@gmail.com>
2019-08-10 22:01:42 +0300

    add check file in directory

---
+++ check_file/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+
+namespace check_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher("C:/Users/kotmavell/Desktop/test");
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            watcher.EnableRaisingEvents = true;
+            Console.ReadKey();
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+    }
+}
